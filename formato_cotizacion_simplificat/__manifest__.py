e8452a99ab23ff7269749b42ccd35a96f2afab04
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-07 10:09:08 -0500

    Add formato_cotizacion_simplificat files

---
+++ formato_cotizacion_simplificat/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Formato Cotizacion SimplificaT',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Adapta el formato de cotización a la identidad visual de SimplificaT',
+    'author': 'DevOps MBA Consultings',
+    'depends': ['formato_cotizacion'],
+    'data': [
+        'views/report_saleorder.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
