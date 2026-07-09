e3dd15c604c708aa1c3b00b2c884ee69d776d1c9
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-09 11:53:10 -0500

    FIX: fix authors for formato_cotizacion and partner_social

--- formato_cotizacion/__manifest__.py
+++ formato_cotizacion/__manifest__.py
@@ -3,7 +3,7 @@
     'version': '18.0.1.0.0',
     'category': 'Sales',
     'summary': 'Adapta el formato de cotización a la identidad visual de SimplificaT | MBA Consultings',
-    'author': 'MBA Consultings',
+    'author': 'MBA Consultings, Brooks Gonzalez',
     'website': 'https://www.mbaconsultings.com',
     'depends': ['sale'],
     'data': [
