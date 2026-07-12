e75af61f0b1c19c69a62cc728974675196c4a28e
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-12 14:37:51 -0500

    Remover copia manual de mba-facturacion-panama (se reemplaza por subtree)

--- mba-facturacion-panama/mba_pa_products/models/dgi_cpbs_segment.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class DGICPBSSegment(models.Model):
-    _name = "dgi.cpbs.segment"
-    _description = "DGI CPBS Segment"
-
-    code = fields.Char(required=True, index=True)
-    name = fields.Char(required=True)
