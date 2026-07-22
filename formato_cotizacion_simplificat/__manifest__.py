0c63e29ac8600c40f18fd41337a2984deea0a4dc
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-07-22 13:13:17 -0500

    chore: add MBA Consultings to module name

--- formato_cotizacion_simplificat/__manifest__.py
+++ formato_cotizacion_simplificat/__manifest__.py
@@ -1,8 +1,8 @@
 {
-    'name': 'Formato Cotizacion',
+    'name': 'Formato Cotización (MBA Consultings)',
     'version': '18.0.1.0.0',
     'license': 'LGPL-3',
-    'summary': 'Formato personalizado de cotización',
+    'summary': 'Formato personalizado de cotización | MBA Consultings',
     'description': 'Base quote format for Sale Orders.',
     'category': 'Sales',
     'author': 'MBA Consultings, Brooks Gonzalez',
