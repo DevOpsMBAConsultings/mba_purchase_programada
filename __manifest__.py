6d2610cdf830639d495394d61b4b26054a878230
DevOpsMBAConsultings <devops@mbaconsultings.com>
2026-08-05 09:24:20 -0500

    Squashed 'mba_purchase_order_format/' content from commit 10b9fada
    git-subtree-dir: mba_purchase_order_format
    git-subtree-split: 10b9fadacbc81edafdb744fefd7f14609b445cea

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'MBA Purchase Order Format',
+    'version': '18.0.1.0.0',
+    'category': 'Purchases',
+    'summary': 'Formato de Orden de Compra personalizado (Azul/Rojo)',
+    'description': 'Formato de Orden de Compra personalizado con diseño corporativo en azul y rojo.',
+    'author': 'MBA Consultings',
+    'depends': ['purchase'],
+    'data': [
+        'views/report_purchaseorder.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
